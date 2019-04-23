940d54e54440c087dbe544bfc8b02d2b37a15c8b
TingYU <ting.yu.cn@gmail.com>
2019-04-23 13:50:37 +0200

    Get all the questionnaires

--- src/main/java/com/questionairv1/dao/QuestionDAOImpl.java
+++ src/main/java/com/questionairv1/dao/QuestionDAOImpl.java
@@ -6,6 +6,7 @@
 package com.questionairv1.dao;
 
 import com.questionairv1.model.Question;
+import com.questionairv1.model.Questionnaire;
 import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
@@ -18,7 +19,38 @@ import java.util.ArrayList;
  */
 public class QuestionDAOImpl implements QuestionDAO{
     Connection connection = ConnectionFactory.getConnection();
+
+    //Get All the questionnaires
+    @Override
+    public ArrayList<Questionnaire> getAllTheQuestionnaires() {
+        ArrayList<Questionnaire> questionnaireList = new ArrayList<>();
+        
+        ResultSet rs;
+        Statement st;
+        String query = "SELECT * FROM questionnaires";
+        
+        try {
+            
+            st = connection.createStatement();
+            rs = st.executeQuery(query);
+
+            while (rs.next() == true) {
+
+                Questionnaire questionnaire = new Questionnaire();
+                questionnaire.setIdQuestnr(rs.getLong(1));
+                questionnaire.setIdClient(rs.getLong(2));
+                questionnaire.setNameQuestnr(rs.getString(3));             
+                
+                questionnaireList.add(questionnaire);
+                
+            }
+            } catch (SQLException ex) {
+            System.out.println(ex);
+        }
+        return questionnaireList;
+    }
     
+    //Get all the questions
     @Override
     public ArrayList<Question> getAllTheQuestions(){
         ArrayList<Question> questionList = new ArrayList<>();
@@ -37,12 +69,13 @@ public class QuestionDAOImpl implements QuestionDAO{
                 Question question = new Question();
                 question.setIdQuest(rs.getLong(1));
                 question.setContentQuest(rs.getString(2));
-                
-                //dont know how to set type
-                
+                question.setTypeQuest(rs.getString(3));                              
                 question.setMin(rs.getLong(4));
                 question.setMax(rs.getLong(5));
                 question.setIdCat(rs.getLong(6));
+                
+                
+
 
                 questionList.add(question);
                 
